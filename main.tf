1df4659ef9ed913a0ef30fe520548f9449784a59
YuJia Lim <63653961+Jia02@users.noreply.github.com>
2023-06-02 19:11:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name   = "jia"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
